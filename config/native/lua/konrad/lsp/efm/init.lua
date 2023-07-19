64e28da7c35b57aa7c9ea9840d1cc07c910f68bd
Konrad Malik <konrad.malik@gmail.com>
2023-07-19 10:22:29 +0200

    refactor: reduce efmutils to only per-plugin

--- config/native/lua/konrad/lsp/efm/init.lua
+++ config/native/lua/konrad/lsp/efm/init.lua
@@ -1,8 +1,45 @@
 -- https://github.com/mattn/efm-langserver
 local utils = require('konrad.utils')
-local efmutils = require('konrad.lsp.efm.utils')
 local binaries = require('konrad.binaries')
 local already_enabled_set = {}
+--
+---@param plugins string[] names of plugins to add, ex. 'prettier'
+function efm_with(plugins)
+    local languages = {}
+    for _, v in ipairs(plugins) do
+        local plugin = require('konrad.lsp.efm.' .. v)
+        for key, value in pairs(plugin) do
+            if languages[key] then
+                languages[key] = utils.concat_lists(languages[key], value)
+            else
+                languages[key] = value
+            end
+        end
+    end
+
+    return {
+        single_file_support = true,
+        filetypes = vim.tbl_keys(languages),
+        init_options = { documentFormatting = true, },
+        settings = {
+            rootMarkers = { '.git/' },
+            languages = languages,
+        },
+    }
+end
+
+-- an example of changing configuration dynamically, but notice that filetypes that neovim registers the lsp for cannot
+-- be changed in that way, just the behavior per filetype
+-- local client = vim.lsp.get_active_clients({ name = 'efm' })[1]
+-- client.notify('workspace/didChangeConfiguration', {
+--     settings = {
+--         languages = {
+--                lua = {
+--                 { formatCommand = "lua-format -i", formatStdin = true },
+--             },
+--         },
+--     },
+-- })
 
 local M = {}
 -- returns config to be put into lspconfig['efm'].setup(config)
@@ -15,7 +52,7 @@ function M.updated_config_for(plugins)
 
     already_enabled_set = vim.tbl_extend("keep", already_enabled_set, plugins_set or {})
     local all_plugins = vim.tbl_keys(already_enabled_set)
-    local settings = efmutils.efm_with(all_plugins)
+    local settings = efm_with(all_plugins)
     return vim.tbl_extend('error', { cmd = { binaries.efm } }, settings)
 end
 
--- config/native/lua/konrad/lsp/efm/utils.lua
+++ config/native/lua/konrad/lsp/efm/utils.lua
@@ -1,4 +1,3 @@
-local utils = require("konrad.utils")
 local M = {}
 
 ---@param fts string[]
@@ -14,43 +13,5 @@ M.make_languages_entry_for_fts = function(fts, entry)
     end
     return vim.tbl_extend("error", unpack(nested))
 end
-
----@param plugins string[] names of plugins to add, ex. 'prettier'
-M.efm_with = function(plugins)
-    local languages = {}
-    for _, v in ipairs(plugins) do
-        local plugin = require('konrad.lsp.efm.' .. v)
-        for key, value in pairs(plugin) do
-            if languages[key] then
-                languages[key] = utils.concat_lists(languages[key], value)
-            else
-                languages[key] = value
-            end
-        end
-    end
-
-    return {
-        single_file_support = true,
-        filetypes = vim.tbl_keys(languages),
-        init_options = { documentFormatting = true, },
-        settings = {
-            rootMarkers = { '.git/' },
-            languages = languages,
-        },
-    }
-end
-
--- an example of changing configuration dynamically, but notice that filetypes that neovim registers the lsp for cannot
--- be changed in that way, just the behavior per filetype
--- local client = vim.lsp.get_active_clients({ name = 'efm' })[1]
--- client.notify('workspace/didChangeConfiguration', {
---     settings = {
---         languages = {
---                lua = {
---                 { formatCommand = "lua-format -i", formatStdin = true },
---             },
---         },
---     },
--- })
 
 return M
