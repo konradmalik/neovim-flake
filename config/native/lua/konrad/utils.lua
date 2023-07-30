eaafcbfe89d319aef46cd4629191a0cc41aafb6f
Konrad Malik <konrad.malik@gmail.com>
2023-07-30 10:53:50 +0200

    refactor: remove unused utils function

--- config/native/lua/konrad/utils.lua
+++ config/native/lua/konrad/utils.lua
@@ -1,21 +1,10 @@
-local utils = {}
-
----@param ... string
----@return boolean
-function utils.has_bins(...)
-    for i = 1, select("#", ...) do
-        if 0 == vim.fn.executable((select(i, ...))) then
-            return false
-        end
-    end
-    return true
-end
+local M = {}
 
 ---@param name string
 ---@param packadds string[]
 ---@param fun function
 ---@param opts table|nil
-function utils.make_enable_command(name, packadds, fun, opts)
+function M.make_enable_command(name, packadds, fun, opts)
     vim.api.nvim_create_user_command(name, function()
         vim.api.nvim_del_user_command(name)
         for _, value in ipairs(packadds) do
@@ -27,9 +16,9 @@ end
 
 ---@param config table Can be obtained by getting the 'client' object in some way and calling 'client.config'
 ---@return boolean
-function utils.is_matching_filetype(config)
+function M.is_matching_filetype(config)
     local ft = vim.bo.filetype or ''
     return ft ~= '' and vim.tbl_contains(config.filetypes or {}, ft)
 end
 
-return utils
+return M
