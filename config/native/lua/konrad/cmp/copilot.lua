ce0cd6f40a18f419bdabddf1824f38e3a722e479
Konrad Malik <konrad.malik@gmail.com>
2023-08-08 23:20:59 +0200

    refactor: simplify copilot cmp

--- config/native/lua/konrad/cmp/copilot.lua
+++ config/native/lua/konrad/cmp/copilot.lua
@@ -5,12 +5,4 @@ require("copilot").setup({
     copilot_node_command = binaries.node,
 })
 
-local fmt = require('copilot_cmp.format')
-require("copilot_cmp").setup({
-    formatters = {
-        label = fmt.format_label_text,
-        -- insert_text = fmt.format_insert_text,
-        insert_text = fmt.remove_existing,
-        preview = fmt.deindent,
-    },
-})
+require("copilot_cmp").setup()
