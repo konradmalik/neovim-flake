e91641b33cea5621f32fdb677186388eba70553c
Konrad Malik <konrad.malik@gmail.com>
2023-09-28 19:48:51 +0200

    feat: update indent_blankline config to 3

--- config/native/plugin/indent-blankline.lua
+++ config/native/plugin/indent-blankline.lua
@@ -1,8 +1,6 @@
-local indent_blankline = require("indent_blankline")
+local indent_blankline = require("ibl")
 local icons = require("konrad.icons").ui
 
-indent_blankline.setup {
-    char = icons.Guide,
-    use_treesitter = true,
-    show_current_context = true,
-}
+indent_blankline.setup({
+    indent = { char = icons.Guide },
+})
