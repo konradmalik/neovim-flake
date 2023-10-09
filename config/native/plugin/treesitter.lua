85fc7491d61ec4752360d76f102800251c5a1b5d
Konrad Malik <konrad.malik@gmail.com>
2023-10-09 22:27:12 +0200

    feat: treesitter textobjects motions

--- config/native/plugin/treesitter.lua
+++ config/native/plugin/treesitter.lua
@@ -12,78 +12,83 @@ require("nvim-treesitter.configs").setup({
     textobjects = {
         select = {
             enable = true,
-            lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+            lookahead = true,
             keymaps = {
                 -- You can use the capture groups defined in textobjects.scm
-                ["aa"] = "@parameter.outer",
-                ["ia"] = "@parameter.inner",
-                ["af"] = "@function.outer",
-                ["if"] = "@function.inner",
-                ["ac"] = "@class.outer",
-                ["ic"] = "@class.inner",
+                ["aa"] = { query = "@parameter.outer", desc = "Select outer part of a parameter/argument" },
+                ["ia"] = { query = "@parameter.inner", desc = "Select inner part of a parameter/argument" },
+
+                ["ai"] = { query = "@conditional.outer", desc = "Select outer part of a conditional" },
+                ["ii"] = { query = "@conditional.inner", desc = "Select inner part of a conditional" },
+
+                ["al"] = { query = "@loop.outer", desc = "Select outer part of a loop" },
+                ["il"] = { query = "@loop.inner", desc = "Select inner part of a loop" },
+
+                ["af"] = { query = "@call.outer", desc = "Select outer part of a function call" },
+                ["if"] = { query = "@call.inner", desc = "Select inner part of a function call" },
+
+                ["am"] = { query = "@function.outer", desc = "Select outer part of a method/function definition" },
+                ["im"] = { query = "@function.inner", desc = "Select inner part of a method/function definition" },
+
+                ["ac"] = { query = "@class.outer", desc = "Select outer part of a class" },
+                ["ic"] = { query = "@class.inner", desc = "Select inner part of a class" },
             },
         },
         move = {
             enable = true,
             set_jumps = true, -- whether to set jumps in the jumplist
             goto_next_start = {
-                ["]m"] = "@function.outer",
-                ["]]"] = "@class.outer",
+                ["]f"] = { query = "@call.outer", desc = "Next function call start" },
+                ["]m"] = { query = "@function.outer", desc = "Next method/function def start" },
+                ["]c"] = { query = "@class.outer", desc = "Next class start" },
+                ["]i"] = { query = "@conditional.outer", desc = "Next conditional start" },
+                ["]l"] = { query = "@loop.outer", desc = "Next loop start" },
             },
             goto_next_end = {
-                ["]M"] = "@function.outer",
-                ["]["] = "@class.outer",
+                ["]F"] = { query = "@call.outer", desc = "Next function call end" },
+                ["]M"] = { query = "@function.outer", desc = "Next method/function def end" },
+                ["]C"] = { query = "@class.outer", desc = "Next class end" },
+                ["]I"] = { query = "@conditional.outer", desc = "Next conditional end" },
+                ["]L"] = { query = "@loop.outer", desc = "Next loop end" },
             },
             goto_previous_start = {
-                ["[m"] = "@function.outer",
-                ["[["] = "@class.outer",
+                ["[f"] = { query = "@call.outer", desc = "Prev function call start" },
+                ["[m"] = { query = "@function.outer", desc = "Prev method/function def start" },
+                ["[c"] = { query = "@class.outer", desc = "Prev class start" },
+                ["[i"] = { query = "@conditional.outer", desc = "Prev conditional start" },
+                ["[l"] = { query = "@loop.outer", desc = "Prev loop start" },
             },
             goto_previous_end = {
-                ["[M"] = "@function.outer",
-                ["[]"] = "@class.outer",
+                ["[F"] = { query = "@call.outer", desc = "Prev function call end" },
+                ["[M"] = { query = "@function.outer", desc = "Prev method/function def end" },
+                ["[C"] = { query = "@class.outer", desc = "Prev class end" },
+                ["[I"] = { query = "@conditional.outer", desc = "Prev conditional end" },
+                ["[L"] = { query = "@loop.outer", desc = "Prev loop end" },
             },
         },
         swap = {
             enable = true,
             swap_next = {
-                ["<leader>a"] = "@parameter.inner",
+                ["<leader>na"] = { query = "@parameter.inner", desc = "swap parameters/argument with next" },
+                ["<leader>nm"] = { query = "@function.outer", desc = "swap function with next" },
             },
             swap_previous = {
-                ["<leader>A"] = "@parameter.inner",
+                ["<leader>pa"] = { query = "@parameter.inner", desc = "Swap parameters/argument with prev" },
+                ["<leader>pm"] = { query = "@function.outer", desc = "swap function with previous" },
             },
         },
     },
 })
 
-require("treesitter-context").setup({
-    enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
-    max_lines = 5, -- How many lines the window should span. Values <= 0 mean no limit.
-    trim_scope = "outer", -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
-    patterns = { -- Match patterns for TS nodes. These get wrapped to match at word boundaries.
-        -- For all filetypes
-        -- Note that setting an entry here replaces all other patterns for this entry.
-        -- By setting the 'default' entry below, you can control which nodes you want to
-        -- appear in the context window.
-        default = {
-            "class",
-            "function",
-            "method",
-            -- 'for', -- These won't appear in the context
-            -- 'while',
-            -- 'if',
-            -- 'switch',
-            -- 'case',
-        },
-        -- Example for a specific filetype.
-        -- If a pattern is missing, *open a PR* so everyone can benefit.
-        --   rust = {
-        --       'impl_item',
-        --   },
-    },
-    exact_patterns = {
-        -- Example for a specific filetype with Lua patterns
-        -- Treat patterns.rust as a Lua pattern (i.e "^impl_item$" will
-        -- exactly match "impl_item" only)
-        -- rust = true,
-    },
-})
+local ts_repeat_move = require("nvim-treesitter.textobjects.repeatable_move")
+
+-- vim way: ; goes to the direction you were moving.
+vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move)
+vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_opposite)
+
+-- Optionally, make builtin f, F, t, T also repeatable with ; and ,
+vim.keymap.set({ "n", "x", "o" }, "f", ts_repeat_move.builtin_f)
+vim.keymap.set({ "n", "x", "o" }, "F", ts_repeat_move.builtin_F)
+vim.keymap.set({ "n", "x", "o" }, "t", ts_repeat_move.builtin_t)
+vim.keymap.set({ "n", "x", "o" }, "T", ts_repeat_move.builtin_T)
+
