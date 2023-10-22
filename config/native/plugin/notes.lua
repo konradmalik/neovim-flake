c56cb91ddc47fd1bbde39ad45d323fe5d319a920
Konrad Malik <konrad.malik@gmail.com>
2023-10-22 18:54:14 +0200

    feat: another keybinding for notes

--- config/native/plugin/notes.lua
+++ config/native/plugin/notes.lua
@@ -4,7 +4,7 @@ end
 
 local open_quicknotes = function()
     local notes_path
-    if vim.fn.has('mac') > 0 then
+    if vim.fn.has("mac") > 0 then
         notes_path = "/Users/konrad/Library/Mobile Documents/iCloud~md~obsidian/Documents"
     else
         notes_path = "/home/konrad/obsidian"
@@ -14,4 +14,4 @@ local open_quicknotes = function()
     vim.cmd("botright vsplit + " .. notes_path)
 end
 
-vim.keymap.set("n", "<leader>n", open_quicknotes, opts_with_desc("Open quick-notes file"))
+vim.keymap.set("n", "<leader>on", open_quicknotes, opts_with_desc("Open quick-notes file"))
