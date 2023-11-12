c30f13294d248318991e106fbfe48a5f52b9fed3
Konrad Malik <konrad.malik@gmail.com>
2023-11-12 17:36:13 +0100

    fix: remove .nvim.lua from skeletons code

--- config/native/plugin/skeletons.lua
+++ config/native/plugin/skeletons.lua
@@ -1,38 +1,30 @@
 local group = vim.api.nvim_create_augroup("personal-skeletons", { clear = true })
-local skeletons_path = require('konrad.skeletons')
+local skeletons_path = require("konrad.skeletons")
 
 local insert_skeleton = function(name)
     vim.cmd("0r " .. skeletons_path .. "/" .. name)
 end
 
 vim.api.nvim_create_autocmd({ "BufNewFile" }, {
-    group    = group,
-    pattern  = ".nvim.lua",
-    callback = function()
-        insert_skeleton(".nvim.lua")
-    end
-})
-
-vim.api.nvim_create_autocmd({ "BufNewFile" }, {
-    group    = group,
-    pattern  = ".tmux.sh",
+    group = group,
+    pattern = ".tmux.sh",
     callback = function()
         insert_skeleton(".tmux.sh")
-    end
+    end,
 })
 
 vim.api.nvim_create_autocmd({ "BufNewFile" }, {
-    group    = group,
-    pattern  = ".editorconfig",
+    group = group,
+    pattern = ".editorconfig",
     callback = function()
         insert_skeleton(".editorconfig")
-    end
+    end,
 })
 
 vim.api.nvim_create_autocmd({ "BufNewFile" }, {
-    group    = group,
-    pattern  = "flake.nix",
+    group = group,
+    pattern = "flake.nix",
     callback = function()
         insert_skeleton("flake.nix")
-    end
+    end,
 })
