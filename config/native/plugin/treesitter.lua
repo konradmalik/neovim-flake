fb0f72fa9c54c171936b254ad3c19c0f6bc923fd
Konrad Malik <konrad.malik@gmail.com>
2023-11-14 15:18:16 +0100

    fix: disable some treesitter's repeatable_move keymaps which break dot-repeat until https://github.com/nvim-treesitter/nvim-treesitter-textobjects/issues/519

--- config/native/plugin/treesitter.lua
+++ config/native/plugin/treesitter.lua
@@ -86,9 +86,9 @@ local ts_repeat_move = require("nvim-treesitter.textobjects.repeatable_move")
 vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move)
 vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_opposite)
 
+-- only enable after https://github.com/nvim-treesitter/nvim-treesitter-textobjects/issues/519 is fixed
 -- Optionally, make builtin f, F, t, T also repeatable with ; and ,
-vim.keymap.set({ "n", "x", "o" }, "f", ts_repeat_move.builtin_f)
-vim.keymap.set({ "n", "x", "o" }, "F", ts_repeat_move.builtin_F)
-vim.keymap.set({ "n", "x", "o" }, "t", ts_repeat_move.builtin_t)
-vim.keymap.set({ "n", "x", "o" }, "T", ts_repeat_move.builtin_T)
-
+-- vim.keymap.set({ "n", "x", "o" }, "f", ts_repeat_move.builtin_f)
+-- vim.keymap.set({ "n", "x", "o" }, "F", ts_repeat_move.builtin_F)
+-- vim.keymap.set({ "n", "x", "o" }, "t", ts_repeat_move.builtin_t)
+-- vim.keymap.set({ "n", "x", "o" }, "T", ts_repeat_move.builtin_T)
