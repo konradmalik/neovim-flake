45bc1a0081f8221c02337f82c46b52896d9ca605
Konrad Malik <konrad.malik@gmail.com>
2023-12-01 13:54:33 +0100

    fix: remove all treesitter repeat as they break default behavior

--- config/native/plugin/treesitter.lua
+++ config/native/plugin/treesitter.lua
@@ -80,13 +80,13 @@ require("nvim-treesitter.configs").setup({
     },
 })
 
-local ts_repeat_move = require("nvim-treesitter.textobjects.repeatable_move")
-
--- vim way: ; goes to the direction you were moving.
-vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move)
-vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_opposite)
-
 -- only enable after https://github.com/nvim-treesitter/nvim-treesitter-textobjects/issues/519 is fixed
+-- local ts_repeat_move = require("nvim-treesitter.textobjects.repeatable_move")
+
+-- -- vim way: ; goes to the direction you were moving.
+-- vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move)
+-- vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_opposite)
+
 -- Optionally, make builtin f, F, t, T also repeatable with ; and ,
 -- vim.keymap.set({ "n", "x", "o" }, "f", ts_repeat_move.builtin_f)
 -- vim.keymap.set({ "n", "x", "o" }, "F", ts_repeat_move.builtin_F)
