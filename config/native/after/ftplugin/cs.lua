2b9cfc11445aa5896babc789787eb15a40130474
Konrad Malik <konrad.malik@gmail.com>
2023-12-28 20:09:33 +0100

    refactor: simplify csharp

--- config/native/after/ftplugin/cs.lua
+++ config/native/after/ftplugin/cs.lua
@@ -1,8 +1,6 @@
 local lsp = require("konrad.lsp")
 local dap = require("konrad.dap")
 
-local csconfig = require("konrad.languages.csharp").current_config()
-
-local lspconfig = require("konrad.lsp.configs." .. csconfig.lsp).config
+local lspconfig = require("konrad.lsp.configs.omnisharp").config
 lsp.start_and_attach(lspconfig)
 dap.initialize("cs")
