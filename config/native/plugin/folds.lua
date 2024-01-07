e7438da25b95ecf1dfa1b00ceff15d2486feb2b7
Konrad Malik <konrad.malik@gmail.com>
2024-01-07 22:15:12 +0100

    feat: re-enable treesitter folding
    freezing bug appears to be fixed in neovim

--- config/native/plugin/folds.lua
+++ config/native/plugin/folds.lua
@@ -1,7 +1,7 @@
 local icons = require("konrad.icons")
 
 -- keep foldcolumn on by default
-vim.wo.foldcolumn = "1"
+vim.wo.foldcolumn = "auto"
 -- custom icons for foldcolumn
 vim.o.fillchars = "eob: ,fold: ,foldopen:"
     .. icons.ui.FoldOpen
@@ -9,13 +9,9 @@ vim.o.fillchars = "eob: ,fold: ,foldopen:"
     .. icons.ui.Guide
     .. ",foldclose:"
     .. icons.ui.FoldClosed
-vim.wo.foldmethod = "indent"
--- TODO this works well, but as of nvim 0.9.1 - if the current file is modified by another process, it will make the
--- whole neovim freeze
--- treesitter based folding
--- vim.wo.foldmethod = "expr"
--- vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
--- needs to be set to some value to not fold everything at start
-vim.wo.foldlevel = 5
--- disable folding at start completely
+vim.wo.foldmethod = "expr"
+vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+vim.wo.foldtext = "v:lua.vim.treesitter.foldtext()"
+-- when enabling, start with this level
+vim.wo.foldlevel = 1
 vim.wo.foldenable = false
