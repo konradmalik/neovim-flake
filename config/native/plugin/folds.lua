7fab03b9c42b356f4a77ea49a7050d12e3f57806
Konrad Malik <konrad.malik@gmail.com>
2024-01-08 14:16:00 +0100

    fix: go back to indent-based folds because of neovim bug when using fugitive

--- config/native/plugin/folds.lua
+++ config/native/plugin/folds.lua
@@ -9,8 +9,12 @@ vim.o.fillchars = "eob: ,fold: ,foldopen:"
     .. icons.ui.Guide
     .. ",foldclose:"
     .. icons.ui.FoldClosed
-vim.wo.foldmethod = "expr"
-vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+-- TODO [0.9.5] treesitter foldexpr now does not freeze on external file modifications
+-- but is unusable with fugitive's Git log command. For some reason it takes ~1 min to open the log
+-- when this is set, so for now still use indent
+vim.wo.foldmethod = "indent"
+-- vim.wo.foldmethod = "expr"
+-- vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
 vim.wo.foldtext = "v:lua.vim.treesitter.foldtext()"
 -- when enabling, start with this level
 vim.wo.foldlevel = 1
