09d8a82173bbe238e63e747ba8c881f36c860f65
Konrad Malik <konrad.malik@gmail.com>
2024-01-08 22:17:13 +0100

    refactor: better spellfile handling

--- config/native/plugin/spell.lua
+++ config/native/plugin/spell.lua
@@ -1,11 +1,18 @@
-local spellfile_path = vim.uv.os_homedir() .. "/Code/github.com/konradmalik/neovim-flake/files/en.utf-8.add"
+local spellfile_path = function()
+    local repo = vim.uv.os_homedir() .. "/Code/github.com/konradmalik/neovim-flake"
+    if vim.fn.isdirectory(repo) == 0 then
+        return vim.fn.stdpath("config") .. "/spell/en.utf-8.add"
+    end
+
+    return repo .. "/files/spell/en.utf-8.add"
+end
 
 vim.opt.spelllang = { "en_us" }
 vim.opt.spell = true
-vim.opt.spellfile = spellfile_path
+vim.opt.spellfile = spellfile_path()
 
 vim.api.nvim_create_user_command("MkSpell", function()
-    vim.cmd("mkspell! " .. spellfile_path)
+    vim.cmd("mkspell! " .. spellfile_path())
 end, {
-    desc = "Regenerates spellfile from " .. spellfile_path,
+    desc = "Regenerates spellfile for " .. spellfile_path(),
 })
