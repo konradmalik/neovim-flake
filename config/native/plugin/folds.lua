0bce719b3c47a2fc010c1b9b3923e591c3e3292a
Konrad Malik <konrad.malik@gmail.com>
2024-01-08 22:46:38 +0100

    refactor: fillchars as lua table

--- config/native/plugin/folds.lua
+++ config/native/plugin/folds.lua
@@ -1,21 +1,22 @@
 local icons = require("konrad.icons")
 
 -- keep foldcolumn on by default
-vim.wo.foldcolumn = "auto"
+vim.opt.foldcolumn = "auto"
 -- custom icons for foldcolumn
-vim.o.fillchars = "eob: ,fold: ,foldopen:"
-    .. icons.ui.FoldOpen
-    .. ",foldsep:"
-    .. icons.ui.Guide
-    .. ",foldclose:"
-    .. icons.ui.FoldClosed
+vim.opt.fillchars = {
+    eob = " ",
+    fold = " ",
+    foldopen = icons.ui.FoldOpen,
+    foldsep = icons.ui.Guide,
+    foldclose = icons.ui.FoldClosed,
+}
 -- TODO [0.9.5] treesitter foldexpr now does not freeze on external file modifications
 -- but is unusable with fugitive's Git log command. For some reason it takes ~1 min to open the log
 -- when this is set, so for now still use indent
-vim.wo.foldmethod = "indent"
--- vim.wo.foldmethod = "expr"
--- vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-vim.wo.foldtext = "v:lua.vim.treesitter.foldtext()"
+vim.opt.foldmethod = "indent"
+-- vim.opt.foldmethod = "expr"
+-- vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+vim.opt.foldtext = "v:lua.vim.treesitter.foldtext()"
 -- when enabling, start with this level
-vim.wo.foldlevel = 1
-vim.wo.foldenable = false
+vim.opt.foldlevel = 1
+vim.opt.foldenable = false
