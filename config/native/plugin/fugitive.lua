ccfd1e34db9a20967290707f0099cbdfc16e814c
Konrad Malik <konrad.malik@gmail.com>
2024-01-09 13:16:18 +0100

    fix: fugitive keybinds are better now

--- config/native/plugin/fugitive.lua
+++ config/native/plugin/fugitive.lua
@@ -3,6 +3,6 @@ local opts_with_desc = function(desc)
     return vim.tbl_extend("error", opts, { desc = "[Fugitive] " .. desc })
 end
 
-vim.keymap.set("v", "<leader>gl", ":Gclog<CR>", opts_with_desc("history of the selected lines"))
-vim.keymap.set("n", "<leader>gl", ":0Gclog<CR>", opts_with_desc("history of the current file"))
-vim.keymap.set("n", "<leader>gL", ":Gclog<CR>", opts_with_desc("commits history of the current file"))
+vim.keymap.set("v", "<leader>gl", ":Gclog!<CR>", opts_with_desc("history of the selected lines"))
+vim.keymap.set("n", "<leader>gl", ":0Gclog!<CR>", opts_with_desc("history of the current file"))
+vim.keymap.set("n", "<leader>gL", ":Gclog! %<CR>", opts_with_desc("commits history of the current file"))
