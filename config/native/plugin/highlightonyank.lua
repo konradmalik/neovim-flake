65aa38751bb01f69fe38494f4bb0d2001b8fe845
Konrad Malik <konrad.malik@gmail.com>
2024-01-13 18:07:45 +0100

    fix: on_yank callback

--- config/native/plugin/highlightonyank.lua
+++ config/native/plugin/highlightonyank.lua
@@ -1,6 +1,8 @@
 local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
 vim.api.nvim_create_autocmd("TextYankPost", {
-    callback = vim.highlight.on_yank,
+    callback = function()
+        vim.highlight.on_yank()
+    end,
     group = highlight_group,
     pattern = "*",
 })
