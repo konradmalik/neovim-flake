ba3683190f5e66d7a9b1e86691a8bb7b3bb8dc70
Konrad Malik <konrad.malik@gmail.com>
2024-01-28 15:00:45 +0100

    refactor: better lightbulb code

--- config/native/lua/konrad/lsp/lightbulb.lua
+++ config/native/lua/konrad/lsp/lightbulb.lua
@@ -1,31 +1,25 @@
+---@type integer?
 local bulb_bufnr = nil
 local icon = ""
+local highlight = "Normal:CursorLineNr"
 
 -- alternative to floating icon
 -- vim.fn.sign_define("bulb", { text = icon })
 -- vim.fn.sign_place(5, "", "bulb", vim.api.nvim_get_current_buf(), { lnum = 6, priority = 100000000000000000 })
 
 --- Get diagnostics (LSP Diagnostic) at the cursor
----
---- Grab the code from https://github.com/neovim/neovim/issues/21985
----
---- TODO:
---- This PR (https://github.com/neovim/neovim/pull/22883) extends
---- vim.diagnostic.get to return diagnostics at cursor directly and even with
---- LSP Diagnostic structure. If it gets merged, simplify this funciton (the
---- code for filter and build can be removed).
+--- (probably can be simplified once this is resolved: https://github.com/neovim/neovim/issues/21985)
 ---@param bufnr integer
 ---@param winnr integer
----@return table A table of LSP Diagnostic
+---@return lsp.Diagnostic[] A table of LSP Diagnostic
 local function get_diagnostic_at_cursor(bufnr, winnr)
     local line, col = unpack(vim.api.nvim_win_get_cursor(winnr)) -- line is 1-based indexing
-    -- Get a table of diagnostics at the current line. The structure of the
-    -- diagnostic item is defined by nvim (see :h diagnostic-structure) to
-    -- describe the information of a diagnostic.
+    ---@type vim.Diagnostic
     local diagnostics = vim.diagnostic.get(bufnr, { lnum = line - 1 }) -- lnum is 0-based indexing
     -- Filter out the diagnostics at the cursor position. And then use each to
     -- build a LSP Diagnostic (see
     -- https://microsoft.github.io/language-server-protocol/specifications/lsp/3.17/specification/#diagnostic)
+    ---@type lsp.Diagnostic[]
     local lsp_diagnostics = {}
     for _, diag in pairs(diagnostics) do
         if diag.col <= col and diag.end_col >= col then
@@ -91,7 +85,7 @@ function M.show(bufnr, winnr)
             style = "minimal",
             noautocmd = true,
         })
-        vim.wo[winid].winhl = "Normal:CursorLineNr"
+        vim.wo[winid].winhl = highlight
         vim.api.nvim_buf_set_lines(bulb_bufnr, 0, 1, false, { icon })
     end)
 end
