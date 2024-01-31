3ed589b5b679d7a6d6bc8bdfb3407213f9fab2ae
Konrad Malik <konrad.malik@gmail.com>
2024-01-31 17:03:08 +0100

    fix: kanagawa highlight captures workaround for upstream tree-sitter changes
    https://github.com/nvim-treesitter/nvim-treesitter/commit/1ae9b0e
    https://github.com/rebelot/kanagawa.nvim/issues/197#issue-2092178700

--- config/native/plugin/colorscheme.lua
+++ config/native/plugin/colorscheme.lua
@@ -4,6 +4,31 @@ require("kanagawa").setup({
         dark = "wave",
         light = "lotus",
     },
+    -- https://github.com/rebelot/kanagawa.nvim/issues/197#issue-2092178700
+    overrides = function()
+        return {
+            -- update kanagawa to handle new treesitter highlight captures
+            ["@string.regexp"] = { link = "@string.regex" },
+            ["@variable.parameter"] = { link = "@parameter" },
+            ["@exception"] = { link = "@exception" },
+            ["@string.special.symbol"] = { link = "@symbol" },
+            ["@markup.strong"] = { link = "@text.strong" },
+            ["@markup.italic"] = { link = "@text.emphasis" },
+            ["@markup.heading"] = { link = "@text.title" },
+            ["@markup.raw"] = { link = "@text.literal" },
+            ["@markup.quote"] = { link = "@text.quote" },
+            ["@markup.math"] = { link = "@text.math" },
+            ["@markup.environment"] = { link = "@text.environment" },
+            ["@markup.environment.name"] = { link = "@text.environment.name" },
+            ["@markup.link.url"] = { link = "Special" },
+            ["@markup.link.label"] = { link = "Identifier" },
+            ["@comment.note"] = { link = "@text.note" },
+            ["@comment.warning"] = { link = "@text.warning" },
+            ["@comment.danger"] = { link = "@text.danger" },
+            ["@diff.plus"] = { link = "@text.diff.add" },
+            ["@diff.minus"] = { link = "@text.diff.delete" },
+        }
+    end,
 })
 
 vim.opt.background = "dark"
