fb8dbe95a3d8377fe701b247f87aebd04cdd786e
Konrad Malik <konrad.malik@gmail.com>
2024-02-12 18:34:48 +0100

    fix: update kanagawa and disable workaround
    Flake lock file updates:
    
    • Updated input 'kanagawa-nvim':
        'github:rebelot/kanagawa.nvim/c19b9023842697ec92caf72cd3599f7dd7be4456' (2023-10-02)
      → 'github:rebelot/kanagawa.nvim/ab41956c4559c3eb21e713fcdf54cda1cb6d5f40' (2024-02-12)

--- config/native/plugin/colorscheme.lua
+++ config/native/plugin/colorscheme.lua
@@ -4,32 +4,6 @@ require("kanagawa").setup({
         dark = "wave",
         light = "lotus",
     },
-    -- https://github.com/rebelot/kanagawa.nvim/issues/197#issue-2092178700
-    overrides = function()
-        return {
-            -- update kanagawa to handle new treesitter highlight captures
-            ["@comment.danger"] = { link = "@text.danger" },
-            ["@comment.note"] = { link = "@text.note" },
-            ["@comment.todo"] = { link = "@text.todo" },
-            ["@comment.warning"] = { link = "@text.warning" },
-            ["@diff.minus"] = { link = "@text.diff.delete" },
-            ["@diff.plus"] = { link = "@text.diff.add" },
-            ["@exception"] = { link = "@exception" },
-            ["@markup.environment"] = { link = "@text.environment" },
-            ["@markup.environment.name"] = { link = "@text.environment.name" },
-            ["@markup.heading"] = { link = "@text.title" },
-            ["@markup.italic"] = { link = "@text.emphasis" },
-            ["@markup.link.label"] = { link = "Identifier" },
-            ["@markup.link.url"] = { link = "Special" },
-            ["@markup.math"] = { link = "@text.math" },
-            ["@markup.quote"] = { link = "@text.quote" },
-            ["@markup.raw"] = { link = "@text.literal" },
-            ["@markup.strong"] = { link = "@text.strong" },
-            ["@string.regexp"] = { link = "@string.regex" },
-            ["@string.special.symbol"] = { link = "@symbol" },
-            ["@variable.parameter"] = { link = "@parameter" },
-        }
-    end,
 })
 
 vim.opt.background = "dark"
