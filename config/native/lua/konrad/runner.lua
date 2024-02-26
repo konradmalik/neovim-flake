59c56362895e292b793d960676c0dceb9a4bd587
Konrad Malik <konrad.malik@gmail.com>
2024-02-26 22:40:12 +0100

    feat: runner window management and scrolling

--- config/native/lua/konrad/runner.lua
+++ config/native/lua/konrad/runner.lua
@@ -1,17 +1,38 @@
-local runner_bufnr
+---@param winid integer
+local function scroll_to_bottom(winid)
+    local last_line = vim.fn.line("$", winid)
+    vim.api.nvim_win_set_cursor(winid, { last_line, 0 })
+end
 
-local function create_buf_and_win_if_needed()
-    if not runner_bufnr or not vim.api.nvim_buf_is_valid(runner_bufnr) then
-        runner_bufnr = vim.api.nvim_create_buf(false, true)
-
-        -- create window only of buffer does not exist
-        -- this allows to eg. move the window to a new tab
-        -- and still reuse it on subsequent runs (winid changes when moved to a tab)
-        vim.api.nvim_open_win(runner_bufnr, false, {
+---tries to find among current windows
+---@param bufnr integer
+---@return integer?
+local function get_or_create_window(bufnr)
+    local winnr
+    local potential_wins = vim.fn.win_findbuf(bufnr) or {}
+    if #potential_wins == 0 then
+        winnr = vim.api.nvim_open_win(bufnr, false, {
             win = 0,
             split = "right",
             noautocmd = true,
         })
+    elseif #potential_wins == 1 then
+        winnr = potential_wins[1]
+    else
+        vim.notify(
+            "found runner_bufnr in multiple windows, shouldn't be possible",
+            vim.log.levels.ERROR
+        )
+        return nil
+    end
+    return winnr
+end
+
+---just one global buffer
+local runner_bufnr
+local function get_or_create_buffer()
+    if not runner_bufnr or not vim.api.nvim_buf_is_valid(runner_bufnr) then
+        runner_bufnr = vim.api.nvim_create_buf(false, true)
     end
 end
 
@@ -37,11 +58,12 @@ local function run(cmd, opts)
     local cwd = opts.cwd
 
     if not bufnr then
-        create_buf_and_win_if_needed()
+        get_or_create_buffer()
         bufnr = runner_bufnr
     elseif bufnr and not vim.api.nvim_buf_is_valid(bufnr) then
         vim.notify("invalid bufnr: " .. bufnr, vim.log.levels.ERROR)
     end
+    get_or_create_window(bufnr)
 
     local cmd_str
     if type(cmd) == "table" then
@@ -61,12 +83,16 @@ local function run(cmd, opts)
     local on_event = function(_, data, event)
         if event == "exit" then
             vim.api.nvim_buf_set_lines(bufnr, -1, -1, false, { "Exit code: " .. data })
+            local winid = get_or_create_window(bufnr)
+            if winid then scroll_to_bottom(winid) end
             return
         end
 
         if data then
             filter_out_trailing_empty_string(data)
             vim.api.nvim_buf_set_lines(bufnr, -1, -1, false, data)
+            local winid = get_or_create_window(bufnr)
+            if winid then scroll_to_bottom(winid) end
         end
     end
 
