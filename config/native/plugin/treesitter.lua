d7f21bec008a7b69ea901e969927df5ff621f768
Konrad Malik <konrad.malik@gmail.com>
2024-03-23 18:04:43 +0100

    feat: treesitter reenable incremental selection

--- config/native/plugin/treesitter.lua
+++ config/native/plugin/treesitter.lua
@@ -1,10 +1,9 @@
 require("nvim-treesitter.configs").setup({
-    ensure_installed = {},
-    ignore_install = {},
-    sync_install = false,
     highlight = {
         enable = true,
-        additional_vim_regex_highlighting = false,
+    },
+    incremental_selection = {
+        enable = true,
     },
     indent = {
         enable = true,
