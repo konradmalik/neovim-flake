119e093a44147587c95dcbd78e5af771e50ac294
Konrad Malik <konrad.malik@gmail.com>
2024-05-26 22:27:30 +0200

    feat: simplify dap

--- config/native/lua/pde/dap/init.lua
+++ config/native/lua/pde/dap/init.lua
@@ -1,3 +1,42 @@
+local setup = function()
+    require("nvim-dap-virtual-text").setup({
+        enabled = true,
+        enabled_commands = true,
+    })
+
+    local dap = require("dap")
+    local dapui = require("dapui")
+
+    local icons = require("pde.icons")
+
+    dapui.setup({
+        icons = {
+            expanded = icons.ui.Expanded,
+            collapsed = icons.ui.Collapsed,
+            current_frame = icons.ui.FoldClosed,
+        },
+    })
+
+    local map = function(lhs, rhs, desc)
+        desc = "[DAP] " .. desc
+        vim.keymap.set("n", lhs, rhs, { desc = desc })
+    end
+
+    map("<F1>", dap.continue, "continue")
+    map("<F2>", dap.step_into, "step into")
+    map("<F3>", dap.step_over, "step over")
+    map("<F4>", dap.step_out, "step out")
+    map("<F5>", dap.step_back, "step back")
+    map("<F12>", dap.restart, "restart")
+    map("<leader>db", dap.toggle_breakpoint, "toggle breakpoint")
+    map("<leader>d?", function() dapui.eval(nil, { enter = true }) end, "evaluate under cursor")
+
+    dap.listeners.before.attach.dapui_config = function() dapui.open() end
+    dap.listeners.before.launch.dapui_config = function() dapui.open() end
+    dap.listeners.before.event_terminated.dapui_config = function() dapui.close() end
+    dap.listeners.before.event_exited.dapui_config = function() dapui.close() end
+end
+
 local initialized = false
 local initialize_once = function()
     if initialized then return end
@@ -8,8 +47,7 @@ local initialize_once = function()
     vim.cmd.packadd("nvim-dap-ui")
     vim.cmd.packadd("nvim-dap-virtual-text")
 
-    require("pde.dap.ui")
-    require("pde.dap.virtual-text")
+    setup()
     require("pde.dap.keymaps")
 
     initialized = true
