50305f0c9811e9d349b0453288cd6251ea3d7a7d
Konrad Malik <konrad.malik@gmail.com>
2024-06-19 09:54:01 +0200

    chore: remove zig_0_12 workaround

--- flake/default.nix
+++ flake/default.nix
@@ -13,19 +13,7 @@
     {
       _module.args.pkgs = import inputs.nixpkgs {
         inherit system;
-        overlays = [
-          # until https://github.com/NixOS/nixpkgs/issues/317055
-          (final: prev: {
-            zig_0_12 = prev.zig_0_12.overrideAttrs (_oldAttrs: {
-              preConfigure = ''
-                CC=$(type -p $CC)
-                CXX=$(type -p $CXX)
-                LD=$(type -p $LD)
-                AR=$(type -p $AR)
-              '';
-            });
-          })
-        ];
+        overlays = [ ];
       };
 
       formatter = pkgs.nixfmt-rfc-style;
