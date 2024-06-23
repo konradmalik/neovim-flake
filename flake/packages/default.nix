1ae47fe5994d795471d67dc8daaa77c0f275b44c
Konrad Malik <konrad.malik@gmail.com>
2024-06-23 22:49:52 +0200

    refactor: vendoredPlugins -> neovimPlugins

--- flake/packages/default.nix
+++ flake/packages/default.nix
@@ -3,29 +3,23 @@
   perSystem =
     { inputs', pkgs, ... }:
     let
-      vendoredPlugins = pkgs.callPackage ./vendoredPlugins.nix {
-        inherit inputs;
+      neovimPlugins = pkgs.callPackages ./neovimPlugins.nix {
+        inherit inputs inputs';
         all-treesitter-grammars = pkgs.vimPlugins.nvim-treesitter.allGrammars;
       };
-      neovimPlugins = vendoredPlugins // {
-        inherit (inputs'.lz-n.packages) lz-n-vimPlugin;
+      nightlyNeovim = inputs'.neovim-nightly-overlay.packages.default;
+      neovim-pde = pkgs.callPackage ./neovim-pde {
+        inherit neovimPlugins;
+        neovim = nightlyNeovim;
       };
     in
     {
-      packages =
-        let
-          nightlyNeovim = inputs'.neovim-nightly-overlay.packages.default;
-          neovim-pde = pkgs.callPackage ./neovim-pde {
-            inherit neovimPlugins;
-            neovim = nightlyNeovim;
-          };
-        in
-        {
-          inherit neovim-pde;
-          neovim-pde-dev = pkgs.callPackage ./neovim-pde-dev.nix { inherit neovim-pde; };
-          default = neovim-pde;
-          config = neovim-pde.passthru.config;
-          nvim-typecheck = pkgs.callPackage ./nvim-typecheck.nix { neovim = nightlyNeovim; };
-        };
+      packages = {
+        inherit neovim-pde;
+        neovim-pde-dev = pkgs.callPackage ./neovim-pde-dev.nix { inherit neovim-pde; };
+        default = neovim-pde;
+        config = neovim-pde.passthru.config;
+        nvim-typecheck = pkgs.callPackage ./nvim-typecheck.nix { neovim = nightlyNeovim; };
+      } // neovimPlugins;
     };
 }
--- flake/packages/neovimPlugins.nix
+++ flake/packages/neovimPlugins.nix
@@ -5,9 +5,10 @@
   neovimUtils,
   all-treesitter-grammars,
   inputs,
+  inputs',
 }:
 let
-  version = "master";
+  version = "latest";
   buildVim =
     {
       name,
@@ -47,6 +48,8 @@ in
 # - does not work for src in buildVimPlugin
 # - plugins internally depend on vimUtils.plenary-nvim and similar either way
 rec {
+  inherit (inputs'.lz-n.packages) lz-n-vimPlugin;
+
   SchemaStore-nvim = buildVim {
     name = "SchemaStore.nvim";
     src = inputs.SchemaStore-nvim;
@@ -57,45 +60,35 @@ rec {
     src = inputs.boole-nvim;
     nvimRequireCheck = "boole";
   };
-  cmp-buffer = (
-    buildVim {
-      name = "cmp-buffer";
-      src = inputs.cmp-buffer;
-      nvimRequireCheck = "cmp_buffer";
-      dependencies = [ nvim-cmp ];
-    }
-  );
-  cmp-nvim-lsp = (
-    buildVim {
-      name = "cmp-nvim-lsp";
-      src = inputs.cmp-nvim-lsp;
-      nvimRequireCheck = "cmp_nvim_lsp";
-      dependencies = [ nvim-cmp ];
-    }
-  );
-  cmp-path = (
-    buildVim {
-      name = "cmp-path";
-      src = inputs.cmp-path;
-      nvimRequireCheck = "cmp_path";
-      dependencies = [ nvim-cmp ];
-    }
-  );
-  cmp_luasnip = (
-    buildVim {
-      name = "cmp_luasnip";
-      src = inputs.cmp_luasnip;
-      dependencies = [ nvim-cmp ];
-    }
-  );
-  friendly-snippets = (
-    buildVim {
-      name = "friendly-snippets";
-      src = inputs.friendly-snippets;
-      nvimRequireCheck = "luasnip.loaders.from_vscode";
-      dependencies = [ luasnip ];
-    }
-  );
+  cmp-buffer = buildVim {
+    name = "cmp-buffer";
+    src = inputs.cmp-buffer;
+    nvimRequireCheck = "cmp_buffer";
+    dependencies = [ nvim-cmp ];
+  };
+  cmp-nvim-lsp = buildVim {
+    name = "cmp-nvim-lsp";
+    src = inputs.cmp-nvim-lsp;
+    nvimRequireCheck = "cmp_nvim_lsp";
+    dependencies = [ nvim-cmp ];
+  };
+  cmp-path = buildVim {
+    name = "cmp-path";
+    src = inputs.cmp-path;
+    nvimRequireCheck = "cmp_path";
+    dependencies = [ nvim-cmp ];
+  };
+  cmp_luasnip = buildVim {
+    name = "cmp_luasnip";
+    src = inputs.cmp_luasnip;
+    dependencies = [ nvim-cmp ];
+  };
+  friendly-snippets = buildVim {
+    name = "friendly-snippets";
+    src = inputs.friendly-snippets;
+    nvimRequireCheck = "luasnip.loaders.from_vscode";
+    dependencies = [ luasnip ];
+  };
   git-conflict-nvim = buildVim {
     name = "git-conflict.nvim";
     src = inputs.git-conflict-nvim;
@@ -137,24 +130,20 @@ rec {
     src = inputs.nvim-dap;
     nvimRequireCheck = "dap";
   };
-  nvim-dap-ui = (
-    buildVim {
-      name = "nvim-dap-ui";
-      src = inputs.nvim-dap-ui;
-      nvimRequireCheck = "dapui";
-      dependencies = [
-        nvim-dap
-        nvim-nio
-      ];
-    }
-  );
-  nvim-dap-virtual-text = (
-    buildVim {
-      name = "nvim-dap-virtual-text";
-      src = inputs.nvim-dap-virtual-text;
-      dependencies = [ nvim-dap ];
-    }
-  );
+  nvim-dap-ui = buildVim {
+    name = "nvim-dap-ui";
+    src = inputs.nvim-dap-ui;
+    nvimRequireCheck = "dapui";
+    dependencies = [
+      nvim-dap
+      nvim-nio
+    ];
+  };
+  nvim-dap-virtual-text = buildVim {
+    name = "nvim-dap-virtual-text";
+    src = inputs.nvim-dap-virtual-text;
+    dependencies = [ nvim-dap ];
+  };
   nvim-luaref = buildVim {
     name = "nvim-luaref";
     src = inputs.nvim-luaref;
@@ -176,13 +165,11 @@ rec {
     src = inputs.nvim-treesitter-context;
     nvimRequireCheck = "treesitter-context";
   };
-  nvim-treesitter-textobjects = (
-    buildVim {
-      name = "nvim-treesitter-textobjects";
-      src = inputs.nvim-treesitter-textobjects;
-      dependencies = [ nvim-treesitter ];
-    }
-  );
+  nvim-treesitter-textobjects = buildVim {
+    name = "nvim-treesitter-textobjects";
+    src = inputs.nvim-treesitter-textobjects;
+    dependencies = [ nvim-treesitter ];
+  };
   nvim-web-devicons = buildVim {
     name = "nvim-web-devicons";
     src = inputs.nvim-web-devicons;
