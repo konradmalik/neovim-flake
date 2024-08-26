567dbc2b3d42d1066c8f96c28fd5862214677fef
Konrad Malik <konrad.malik@gmail.com>
2024-08-26 16:40:34 +0200

    feat: very simple completefunc for experimentation

---
+++ config/native/lua/pde/incomplete/init.lua
@@ -0,0 +1,47 @@
+local M = {}
+
+---@class CompleteItem
+---@field word string actual completion value
+---@field abbr string menu entry
+---@field menu string menu entry label
+---@field info? string additional info displayed next to menu entry
+
+---@class CompleteDict
+---@field words string|CompleteItem
+---@field refresh? "always"|nil
+
+function M.setup() vim.o.completefunc = "v:lua.require'pde.incomplete'.completefunc" end
+
+---Exemplary stuff for fun and learning
+---@param findstart integer
+---@param base string
+---@return integer|CompleteItem
+function M.completefunc(findstart, base)
+    if findstart == 1 and base == "" then
+        -- column where completion starts
+        -- eg. return 0 for start of the line
+        -- return -1 for cursor column
+        return -1
+    end
+
+    return {
+        ---@type CompleteItem[]
+        words = {
+            {
+                word = "¯\\_(ツ)_/¯",
+                abbr = "shrug",
+                menu = "ascii emoji",
+                info = "when you have nothing better to say",
+            },
+            {
+                word = "(╯°□°)╯彡┻━┻",
+                abbr = "rageflip",
+                menu = "ascii emoji",
+                info = "when you have enough",
+            },
+        },
+        refresh = "always",
+    }
+end
+
+return M
--- config/native/plugin/completion.lua
+++ config/native/plugin/completion.lua
@@ -1,2 +1,5 @@
 vim.opt.shortmess:append("c")
 vim.opt.completeopt = { "menuone", "popup", "noinsert", "noselect", "fuzzy" }
+
+local incomplete = require("pde.incomplete")
+incomplete.setup()
