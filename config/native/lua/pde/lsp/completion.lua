f72a20fbd80564f76d5efc14c452b589bd2238ba
Konrad Malik <konrad.malik@gmail.com>
2024-08-26 11:41:29 +0200

    feat: completion keybinds for better experience

--- config/native/lua/pde/lsp/completion.lua
+++ config/native/lua/pde/lsp/completion.lua
@@ -38,6 +38,13 @@ local function initialize_once()
     initialized = true
 end
 
+local function pumvisible() return tonumber(vim.fn.pumvisible()) ~= 0 end
+
+---@param keys string
+local function feedkeys(keys)
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(keys, true, false, true), "n", true)
+end
+
 local M = {}
 
 ---@param client vim.lsp.Client
@@ -45,6 +52,48 @@ local M = {}
 M.enable = function(client, bufnr)
     initialize_once()
     vim.lsp.completion.enable(true, client.id, bufnr, { autotrigger = true })
+
+    ---@param mode string|string[]
+    ---@param lhs string
+    ---@param rhs string|function
+    ---@param desc string
+    local function keymap(mode, lhs, rhs, desc)
+        vim.keymap.set(
+            mode,
+            lhs,
+            rhs,
+            { desc = "[Completion] " .. desc, buffer = bufnr, expr = true }
+        )
+    end
+
+    -- completion is triggered only on inserting new characters,
+    -- if we delete char to adjust the match, popup disappears
+    -- this solves it
+    for _, keys in ipairs({ "<BS>", "<C-h>" }) do
+        keymap("i", keys, function()
+            if pumvisible() then
+                feedkeys(keys)
+                vim.lsp.completion.trigger()
+                return
+            end
+            feedkeys(keys)
+        end, "Remove previous character and trigger LSP completion if needed")
+    end
+
+    -- Trigger LSP completion.
+    -- If there's none, fallback to vanilla omnifunc.
+    -- if there's none, use buffer completion
+    keymap("i", "<C-Space>", function()
+        if next(vim.lsp.get_clients({ bufnr = bufnr })) then
+            vim.lsp.completion.trigger()
+        else
+            if vim.bo.omnifunc == "" then
+                feedkeys("<C-x><C-n>")
+            else
+                feedkeys("<C-x><C-o>")
+            end
+        end
+    end, "Smart completion trigger")
 end
 
 ---@param client vim.lsp.Client
