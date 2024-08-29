4f29a4a34e9f4511d7b8731886c7e303da4321c6
Konrad Malik <konrad.malik@gmail.com>
2024-08-29 11:45:57 +0200

    fix: golangci_lint typo

--- config/native/after/ftplugin/go.lua
+++ config/native/after/ftplugin/go.lua
@@ -6,6 +6,6 @@ local lsp = require("pde.lsp")
 
 lsp.start(require("pde.lsp.configs.gopls").config(bufnr), { bufnr = bufnr })
 lsp.start(
-    require("pde.lsp.configs.efm").config_from_single("golangci-lint", bufnr),
+    require("pde.lsp.configs.efm").config_from_single("golangci_lint", bufnr),
     { bufnr = bufnr }
 )
