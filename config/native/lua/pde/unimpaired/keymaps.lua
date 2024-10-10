6152078396c0dea41a213e64e64f6f4c2e9fb886
Konrad Malik <konrad.malik@gmail.com>
2024-10-10 11:15:00 +0200

    refactor: remove unimpaired mappings in favor of new defaults
    https://github.com/neovim/neovim/pull/28525

--- config/native/lua/pde/unimpaired/keymaps.lua
+++ config/native/lua/pde/unimpaired/keymaps.lua
@@ -1,64 +1,8 @@
 local funs = require("pde.unimpaired.functions")
 
 -- movement
-vim.keymap.set("n", "[a", funs.previous, funs.opts_with_desc("jump to previous file in arglist"))
-vim.keymap.set("n", "]a", funs.next, funs.opts_with_desc("jump to next file in arglist"))
-vim.keymap.set("n", "[A", funs.first, funs.opts_with_desc("jump to first file in arglist"))
-vim.keymap.set("n", "]A", funs.last, funs.opts_with_desc("jump to last file in arglist"))
-
-vim.keymap.set("n", "[b", funs.bprevious, funs.opts_with_desc("jump to previous buffer"))
-vim.keymap.set("n", "]b", funs.bnext, funs.opts_with_desc("jump to next buffer"))
-vim.keymap.set("n", "[B", funs.bfirst, funs.opts_with_desc("jump to first buffer"))
-vim.keymap.set("n", "]B", funs.blast, funs.opts_with_desc("jump to last buffer"))
-
-vim.keymap.set("n", "[l", funs.lprevious, funs.opts_with_desc("jump to previous entry in loclist"))
-vim.keymap.set("n", "]l", funs.lnext, funs.opts_with_desc("jump to next entry in loclist"))
-vim.keymap.set("n", "[L", funs.lfirst, funs.opts_with_desc("jump to first entry in loclist"))
-vim.keymap.set("n", "]L", funs.llast, funs.opts_with_desc("jump to last entry in loclist"))
-vim.keymap.set(
-    "n",
-    "[<C-l>",
-    funs.lpfile,
-    funs.opts_with_desc("jump to last entry of previous file in loclist")
-)
-vim.keymap.set(
-    "n",
-    "]<C-l>",
-    funs.lnfile,
-    funs.opts_with_desc("jump to first entry of next file in loclist")
-)
-
-vim.keymap.set("n", "[q", funs.cprevious, funs.opts_with_desc("jump to previous entry in qflist"))
-vim.keymap.set("n", "]q", funs.cnext, funs.opts_with_desc("jump to next entry in qflist"))
-vim.keymap.set("n", "[Q", funs.cfirst, funs.opts_with_desc("jump to first entry in qflist"))
-vim.keymap.set("n", "]Q", funs.clast, funs.opts_with_desc("jump to last entry in qflist"))
-vim.keymap.set(
-    "n",
-    "[<C-q>",
-    funs.cpfile,
-    funs.opts_with_desc("jump to last entry of previous file in qflist")
-)
-vim.keymap.set(
-    "n",
-    "]<C-q>",
-    funs.cnfile,
-    funs.opts_with_desc("jump to first entry of next file in qflist")
-)
-
 vim.keymap.set("n", "<leader>q", funs.toggle_qflist, funs.opts_with_desc("Toggle Quickfix List"))
 vim.keymap.set("n", "<leader>l", funs.toggle_llist, funs.opts_with_desc("Toggle Local List"))
-
-vim.keymap.set("n", "[t", funs.tprevious, funs.opts_with_desc("jump to previous matching tag"))
-vim.keymap.set("n", "]t", funs.tnext, funs.opts_with_desc("jump to next matching tag"))
-vim.keymap.set("n", "[T", funs.tfirst, funs.opts_with_desc("jump to first matching tag"))
-vim.keymap.set("n", "]T", funs.tlast, funs.opts_with_desc("jump to last matching tag"))
-vim.keymap.set(
-    "n",
-    "[<C-t>",
-    funs.ptprevious,
-    funs.opts_with_desc(":tprevious in the preview window")
-)
-vim.keymap.set("n", "]<C-t>", funs.ptnext, funs.opts_with_desc(":tnext in the preview window"))
 
 vim.keymap.set(
     "n",
