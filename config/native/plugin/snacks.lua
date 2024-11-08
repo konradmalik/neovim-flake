efd4eebca370084f3bbfed410248b7f5ccadcb1e
Konrad Malik <konrad.malik@gmail.com>
2024-11-08 12:09:14 +0100

    feat: add custom remote_patterns for gitbrowse and configure

---
+++ config/native/plugin/snacks.lua
@@ -0,0 +1,42 @@
+local snacks = require("snacks")
+snacks.setup({
+    bigfile = { enabled = true },
+    gitbrowse = {
+        remote_patterns = {
+            -- TODO can we somehow just get this?
+            { "^(https?://.*)%.git$", "%1" },
+            { "^git@(.+):(.+)%.git$", "https://%1/%2" },
+            { "^git@(.+):(.+)$", "https://%1/%2" },
+            { "^git@(.+)/(.+)$", "https://%1/%2" },
+            { "^ssh://git@(.*)$", "https://%1" },
+            { "^ssh://([^:/]+)(:%d+)/(.*)$", "https://%1/%3" },
+            { "^ssh://([^/]+)/(.*)$", "https://%1/%2" },
+            { "ssh%.dev%.azure%.com/v3/(.*)/(.*)$", "dev.azure.com/%1/_git/%2" },
+            { "^https://%w*@(.*)", "https://%1" },
+            { "^git@(.*)", "https://%1" },
+            { ":%d+", "" },
+            { "%.git$", "" },
+            -- custom entries
+            { "repositories%.gitlab%.cerebredev", "gitlab.cerebredev" },
+        },
+        url_patterns = {
+            ["gitlab.cerebredev.com"] = {
+                branch = "/-/tree/{branch}",
+                file = "/-/blob/{branch}/{file}#L{line}",
+            },
+        },
+    },
+    notifier = { enabled = false },
+    quickfile = { enabled = true },
+    statuscolumn = { enabled = true },
+    words = { enabled = false },
+})
+
+local opts_with_desc = function(desc) return { desc = "[snacks] " .. desc } end
+
+vim.keymap.set(
+    "n",
+    "<leader>go",
+    snacks.gitbrowse.open,
+    opts_with_desc("open current file in the browser")
+)
