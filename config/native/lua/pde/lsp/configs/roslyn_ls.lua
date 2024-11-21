520df796579f2b70ffee16934459b5c3b902e984
Konrad Malik <konrad.malik@gmail.com>
2024-11-21 18:35:30 +0100

    feat: update roslyn settings

--- config/native/lua/pde/lsp/configs/roslyn_ls.lua
+++ config/native/lua/pde/lsp/configs/roslyn_ls.lua
@@ -76,6 +76,14 @@ end
 local function get_config()
     return {
         settings = {
+            ["csharp|code_lens"] = {
+                dotnet_enable_references_code_lens = false,
+            },
+            ["csharp|completion"] = {
+                dotnet_provide_regex_completions = false,
+                dotnet_show_completion_items_from_unimported_namespaces = true,
+                dotnet_show_name_completion_suggestions = true,
+            },
             ["csharp|inlay_hints"] = {
                 csharp_enable_inlay_hints_for_implicit_object_creation = true,
                 csharp_enable_inlay_hints_for_implicit_variable_types = true,
