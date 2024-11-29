22c5969e044490370e5167839047f336a2f905cb
Konrad Malik <konrad.malik@gmail.com>
2024-11-29 10:40:14 +0100

    chore: add dotnet 6 and 7 to permittedInsecurePackages

--- flake/default.nix
+++ flake/default.nix
@@ -13,6 +13,15 @@
     {
       _module.args.pkgs = import inputs.nixpkgs {
         inherit system;
+        config = {
+          # from roslyn-ls
+          permittedInsecurePackages = [
+            "dotnet-core-combined"
+            "dotnet-sdk-6.0.428"
+            "dotnet-sdk-7.0.410"
+            "dotnet-sdk-wrapped-6.0.428"
+          ];
+        };
         overlays = [
           inputs.gen-luarc.overlays.default
           inputs.neorocks.overlays.default
