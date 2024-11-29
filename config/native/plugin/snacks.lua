a5f77dd838d14ec79126a700b06cf53adef1c651
Konrad Malik <konrad.malik@gmail.com>
2024-11-29 18:39:06 +0100

    feat: use new snacks config function

--- config/native/plugin/snacks.lua
+++ config/native/plugin/snacks.lua
@@ -2,29 +2,16 @@ local snacks = require("snacks")
 snacks.setup({
     bigfile = { enabled = true },
     gitbrowse = {
-        remote_patterns = {
-            -- order matters
-            { "^(https?://.*)%.git$", "%1" },
-            { "^git@(.+):(.+)%.git$", "https://%1/%2" },
-            { "^git@(.+):(.+)$", "https://%1/%2" },
-            { "^git@(.+)/(.+)$", "https://%1/%2" },
-            { "^ssh://git@(.*)$", "https://%1" },
-            { "^ssh://([^:/]+)(:%d+)/(.*)$", "https://%1/%3" },
-            { "^ssh://([^/]+)/(.*)$", "https://%1/%2" },
-            { "ssh%.dev%.azure%.com/v3/(.*)/(.*)$", "dev.azure.com/%1/_git/%2" },
-            { "^https://%w*@(.*)", "https://%1" },
-            { "^git@(.*)", "https://%1" },
-            { ":%d+", "" },
-            { "%.git$", "" },
-            -- custom entries
-            { "repositories%.gitlab%.cerebredev", "gitlab.cerebredev" },
-        },
-        url_patterns = {
-            ["gitlab.cerebredev.com"] = {
+        config = function(opts, _)
+            table.insert(
+                opts.remote_patterns,
+                { "repositories%.gitlab%.cerebredev", "gitlab.cerebredev" }
+            )
+            opts.url_patterns["gitlab.cerebredev.com"] = {
                 branch = "/-/tree/{branch}",
                 file = "/-/blob/{branch}/{file}#L{line}",
-            },
-        },
+            }
+        end,
     },
 })
 
