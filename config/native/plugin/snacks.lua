6ccc076019cffc16ea2784c26b39cb8c7d2f7062
Konrad Malik <konrad.malik@gmail.com>
2024-12-10 11:15:33 +0100

    fix(snacks): git browse for custom gitlab

--- config/native/plugin/snacks.lua
+++ config/native/plugin/snacks.lua
@@ -3,25 +3,16 @@ snacks.setup({
     bigfile = { enabled = true },
     gitbrowse = {
         config = function(opts, _)
-            table.insert(
-                opts.remote_patterns,
-                { "repositories%.gitlab%.cerebredev", "gitlab.cerebredev" }
-            )
-            opts.url_patterns["gitlab.cerebredev.com"] = {
-                branch = "/-/tree/{branch}",
-                file = "/-/blob/{branch}/{file}#L{line}",
-            }
+            table.insert(opts.remote_patterns, { "repositories%.gitlab%.cerebredev", "gitlab.cerebredev" })
+            opts.url_patterns["gitlab%.cerebredev%.com"] = opts.url_patterns["gitlab%.com"]
         end,
     },
 })
 
-local opts_with_desc = function(desc) return { desc = "[snacks] " .. desc } end
+local opts_with_desc = function(desc)
+    return { desc = "[snacks] " .. desc }
+end
 
-vim.keymap.set(
-    "n",
-    "<leader>go",
-    snacks.gitbrowse.open,
-    opts_with_desc("open current file in the browser")
-)
+vim.keymap.set({ "n", "v" }, "<leader>go", snacks.gitbrowse.open, opts_with_desc("open current file in the browser"))
 
 vim.keymap.set("n", "grf", snacks.rename.rename_file, opts_with_desc("Rename current file"))
