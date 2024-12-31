1244453128eae3c268c2bccdc009698cdeb294f2
Konrad Malik <konrad.malik@gmail.com>
2024-12-31 13:20:43 +0100

    chore: add link to PR improving vim.lsp.config

--- config/nvim/plugin/roslyn_ls.lua
+++ config/nvim/plugin/roslyn_ls.lua
@@ -2,8 +2,7 @@ local binaries = require("pde.binaries")
 
 require("roslyn").setup({
     exe = binaries.roslyn_ls(),
-    -- TODO after https://github.com/neovim/neovim/issues/31640
+    -- TODO after https://github.com/neovim/neovim/issues/31640, https://github.com/neovim/neovim/pull/31771
     -- config = vim.lsp.config.roslyn,
     config = require("pde.lsp.configs.roslyn"),
 })
-
