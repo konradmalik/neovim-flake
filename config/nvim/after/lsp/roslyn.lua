121f2b63670bcc854919f6f2781165a2fb99ec65
Konrad Malik <konrad.malik@gmail.com>
2025-01-22 18:09:13 +0100

    fix: roslyn test selection properly handles file_scoped_namespace_declaration

--- config/nvim/after/lsp/roslyn.lua
+++ config/nvim/after/lsp/roslyn.lua
@@ -29,30 +29,36 @@ end
 
 ---@param root_node TSNode
 ---@return TSNode?
-local function get_class_name_node(root_node)
+local function get_class_node(root_node)
     ---@type TSNode?
     local class_node = root_node
     while class_node and class_node:type() ~= "class_declaration" do
         class_node = class_node:parent()
     end
 
-    if class_node then return class_node:field("name")[1] end
+    return class_node
 end
 
----@param root_node TSNode
+---@param class_node TSNode
 ---@return TSNode?
-local function get_namespace_name_node(root_node)
+local function get_namespace_node(class_node)
     ---@type TSNode?
-    local ns_node = root_node
-    while
-        ns_node
-        and ns_node:type() ~= "namespace_declaration"
-        and ns_node:type() ~= "file_scoped_namespace_declaration"
-    do
+    local ns_node = class_node
+    -- namespace_declaration has a tree structure -> class is defined inside
+    while ns_node and ns_node:type() ~= "namespace_declaration" do
         ns_node = ns_node:parent()
     end
 
-    if ns_node then return ns_node:field("name")[1] end
+    if ns_node then return ns_node end
+
+    ns_node = class_node
+    -- file_scoped_namespace_declaration has a list structure -> class is defined below
+    -- so we need to check the siblings for namespace
+    while ns_node and ns_node:type() ~= "file_scoped_namespace_declaration" do
+        ns_node = ns_node:prev_sibling()
+    end
+
+    return ns_node
 end
 
 ---@param bufnr any
@@ -114,16 +120,30 @@ return {
             end
 
             local root_name = vim.treesitter.get_node_text(root_node, bufnr)
-            local class_name_node = get_class_name_node(root_node)
+            local class_node = get_class_node(root_node)
+
+            if not class_node then
+                vim.notify(
+                    "cannot find class node for root " .. vim.inspect(root_name),
+                    vim.log.levels.ERROR
+                )
+                return
+            end
+
+            local class_name_node = class_node:field("name")[1]
+            -- if class_name_node == root_node then this is a test class
             if class_name_node and class_name_node:id() ~= root_node:id() then
                 local class_name = vim.treesitter.get_node_text(class_name_node, bufnr)
                 root_name = class_name .. "." .. root_name
             end
 
-            local ns_name_node = get_namespace_name_node(class_name_node or root_node)
-            if ns_name_node then
-                local ns_name = vim.treesitter.get_node_text(ns_name_node, bufnr)
-                root_name = ns_name .. "." .. root_name
+            local ns_node = get_namespace_node(class_node)
+            if ns_node then
+                local ns_name_node = ns_node:field("name")[1]
+                if ns_name_node then
+                    local ns_name = vim.treesitter.get_node_text(ns_name_node, bufnr)
+                    root_name = ns_name .. "." .. root_name
+                end
             end
 
             local client = vim.lsp.get_client_by_id(ctx.client_id)
