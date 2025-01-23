a75c125f120fd9b83725f55d7b747b4f2f72c8b6
Konrad Malik <konrad.malik@gmail.com>
2025-01-23 17:10:04 +0100

    feat: expose neovim-pde-dev as an app

--- flake/apps.nix
+++ flake/apps.nix
@@ -2,9 +2,16 @@
   perSystem =
     { pkgs, self', ... }:
     {
-      apps.default = {
-        type = "app";
-        program = pkgs.lib.getExe self'.packages.neovim-pde;
+      apps = {
+        default = self'.apps.neovim-pde;
+        neovim-pde = {
+          type = "app";
+          program = pkgs.lib.getExe self'.packages.neovim-pde;
+        };
+        neovim-pde-dev = {
+          type = "app";
+          program = pkgs.lib.getExe self'.packages.neovim-pde-dev;
+        };
       };
     };
 }
