ae3a28933665647cc90185b8ba97e18774604d84
Konrad Malik <konrad.malik@gmail.com>
2025-02-11 08:51:49 +0100

    feat: integrate lsp-rename with oil.nvim

--- config/nvim/plugin/oil.lua
+++ config/nvim/plugin/oil.lua
@@ -1,3 +1,17 @@
 require("oil").setup()
 
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+
+local group = vim.api.nvim_create_augroup("oil-lsp-rename", { clear = true })
+vim.api.nvim_create_autocmd("User", {
+    group = group,
+    pattern = "OilActionsPost",
+    callback = function(event)
+        if event.data.actions.type == "move" then
+            require("rename").on_rename_file(
+                event.data.actions.src_url,
+                event.data.actions.dest_url
+            )
+        end
+    end,
+})
