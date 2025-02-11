35409027373048a66a1c6e3f9c0c59f06fa620b6
Konrad Malik <konrad.malik@gmail.com>
2025-02-11 08:51:39 +0100

    style: small rearrangement

--- config/nvim/plugin/highlightonyank.lua
+++ config/nvim/plugin/highlightonyank.lua
@@ -1,6 +1,6 @@
-local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
+local group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
 vim.api.nvim_create_autocmd("TextYankPost", {
-    callback = function() vim.highlight.on_yank() end,
-    group = highlight_group,
+    group = group,
     pattern = "*",
+    callback = function() vim.highlight.on_yank() end,
 })
