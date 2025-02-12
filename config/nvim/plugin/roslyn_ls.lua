dea5fa3dfb58ded01cd5efcf63faeca7440924d5
Konrad Malik <konrad.malik@gmail.com>
2025-02-12 22:32:20 +0100

    fix: roslyn config name

--- config/nvim/plugin/roslyn_ls.lua
+++ config/nvim/plugin/roslyn_ls.lua
@@ -2,5 +2,5 @@ local binaries = require("pde.binaries")
 
 require("roslyn").setup({
     exe = binaries.roslyn_ls(),
-    config = vim.lsp.config.roslyn,
+    config = vim.lsp.config.roslyn_ls,
 })
