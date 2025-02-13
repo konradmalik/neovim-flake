d2f87d54e4caf85a923acddbf9cd1e6b33a6cf8e
Konrad Malik <konrad.malik@gmail.com>
2025-02-13 21:45:12 +0100

    feat: oil.nvim already calls rename events

--- config/nvim/plugin/oil.lua
+++ config/nvim/plugin/oil.lua
@@ -1,17 +1,3 @@
 require("oil").setup()
 
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
-
-local group = vim.api.nvim_create_augroup("oil-lsp-rename", { clear = true })
-vim.api.nvim_create_autocmd("User", {
-    group = group,
-    pattern = "OilActionsPost",
-    callback = function(event)
-        if event.data.actions.type == "move" then
-            require("pde.lsp.rename").on_rename_file(
-                event.data.actions.src_url,
-                event.data.actions.dest_url
-            )
-        end
-    end,
-})
