1dcf0a02faa960092aa219e05d43c794248ad2c6
Konrad Malik <konrad.malik@gmail.com>
2025-02-19 11:43:51 +0100

    feat: handy oil keymaps and settings

--- config/nvim/plugin/oil.lua
+++ config/nvim/plugin/oil.lua
@@ -1,3 +1,17 @@
-require("oil").setup()
+local oil = require("oil")
+oil.setup({
+    lsp_file_methods = {
+        timeout_ms = 5000,
+        autosave_changes = "unmodified",
+    },
+    keymaps = {
+        ["gd"] = function() oil.set_columns({ "icon", "permissions", "size", "mtime" }) end,
+        ["<C-q>"] = {
+            "actions.send_to_qflist",
+            desc = "Send files to QF list. If search is active, send only matching.",
+            opts = { only_matching_search = true },
+        },
+    },
+})
 
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
