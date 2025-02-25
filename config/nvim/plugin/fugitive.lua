4ae7fd8e17746b02ebf19dca835d3569de19ae4b
Konrad Malik <konrad.malik@gmail.com>
2025-02-25 08:50:11 +0100

    feat: improve fugitive keybinds, change gitsigns

--- config/nvim/plugin/fugitive.lua
+++ config/nvim/plugin/fugitive.lua
@@ -1,10 +1,20 @@
-local opts_with_desc = function(desc) return { desc = "[fUgitive] " .. desc } end
+local opts_with_desc = function(desc) return { desc = "[fuGitive] " .. desc } end
 
-vim.keymap.set("v", "<leader>ul", ":Gclog!<CR>", opts_with_desc("history of the selected lines"))
-vim.keymap.set("n", "<leader>ul", ":0Gclog!<CR>", opts_with_desc("history of the current file"))
+vim.keymap.set(
+    "v",
+    "<leader>gh",
+    ":Gllog!<CR>",
+    opts_with_desc("history of the selected lines in loclist")
+)
 vim.keymap.set(
     "n",
-    "<leader>uL",
-    ":Gclog! %<CR>",
-    opts_with_desc("commits history of the current file")
+    "<leader>gh",
+    ":0Gllog!<CR>",
+    opts_with_desc("history of the current file in loclist")
+)
+vim.keymap.set(
+    "n",
+    "<leader>gvl",
+    ":vertical Git log -p -- %<CR>",
+    opts_with_desc("current file's commits in a split")
 )
