f494efd219f78698ec2730ad08f916ddf6a18f45
Konrad Malik <konrad.malik@gmail.com>
2025-02-25 08:44:51 +0100

    feat: use foldexpr from core

--- config/nvim/plugin/folds.lua
+++ config/nvim/plugin/folds.lua
@@ -6,10 +6,7 @@ vim.opt.fillchars = {
     foldclose = "",
 }
 vim.o.foldmethod = "expr"
--- TODO: lua-based treesitter's foldexpr is unusable with fugitive's Git log command.
--- For some reason it takes 1-2 mins to open the log when this is set.
--- vim.o.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-vim.o.foldexpr = "nvim_treesitter#foldexpr()"
+vim.o.foldexpr = "v:lua.vim.treesitter.foldexpr()"
 vim.o.foldtext = "v:lua.vim.treesitter.foldtext()"
 -- when enabling, start with this level
 vim.o.foldlevel = 99
