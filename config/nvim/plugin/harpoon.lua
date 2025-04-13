84ed2523bd3694672e746b167f24a96201f88f73
Konrad Malik <konrad.malik@gmail.com>
2025-04-13 20:35:28 +0200

    feat: better harpoon keymaps

--- config/nvim/plugin/harpoon.lua
+++ config/nvim/plugin/harpoon.lua
@@ -2,10 +2,45 @@ local harpoon = require("harpoon")
 
 harpoon:setup()
 
-vim.keymap.set("n", "<leader>ha", function() harpoon:list():add() end)
-vim.keymap.set("n", "<leader>hq", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+---@param desc string
+---@return table
+local function opts_with_desc(desc) return { desc = "[hArpoon] " .. desc } end
 
-vim.keymap.set("n", "<leader>hj", function() harpoon:list():select(1) end)
-vim.keymap.set("n", "<leader>hk", function() harpoon:list():select(2) end)
-vim.keymap.set("n", "<leader>hl", function() harpoon:list():select(3) end)
-vim.keymap.set("n", "<leader>h;", function() harpoon:list():select(4) end)
+vim.keymap.set(
+    "n",
+    "<leader>aa",
+    function() harpoon:list():add() end,
+    opts_with_desc("add to list")
+)
+vim.keymap.set(
+    "n",
+    "<leader>aq",
+    function() harpoon.ui:toggle_quick_menu(harpoon:list()) end,
+    opts_with_desc("toggle quick menu")
+)
+
+vim.keymap.set(
+    "n",
+    "<leader>aj",
+    function() harpoon:list():select(1) end,
+    opts_with_desc("first entry")
+)
+vim.keymap.set(
+    "n",
+    "<leader>ak",
+    function() harpoon:list():select(2) end,
+    opts_with_desc("second entry")
+)
+vim.keymap.set(
+    "n",
+    "<leader>al",
+    function() harpoon:list():select(3) end,
+    opts_with_desc("third entry")
+)
+vim.keymap.set(
+    "n",
+    "<leader>a;",
+    function() harpoon:list():select(4) end,
+    opts_with_desc("fourth entry")
+)
+
