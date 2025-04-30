52e466070a0872bd6feaa905603570b9737a51c1
Konrad Malik <konrad.malik@gmail.com>
2025-04-30 10:41:13 +0200

    fix: use new on_yank

--- config/nvim/plugin/highlightonyank.lua
+++ config/nvim/plugin/highlightonyank.lua
@@ -2,5 +2,5 @@ local group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
 vim.api.nvim_create_autocmd("TextYankPost", {
     group = group,
     pattern = "*",
-    callback = function() vim.highlight.on_yank() end,
+    callback = function() vim.hl.on_yank({ higroup = "Visual", timeout = 300 }) end,
 })
