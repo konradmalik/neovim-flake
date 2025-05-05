e160171893d6b803f7c687ed0aed50eeae8d5cd8
Konrad Malik <konrad.malik@gmail.com>
2025-05-05 10:50:36 +0200

    feat: use new lspconfig commands

--- config/nvim/lua/pde/lsp/commands.lua
+++ config/nvim/lua/pde/lsp/commands.lua
@@ -8,55 +8,6 @@ local function parse_int_arg(args)
     return nil
 end
 
----@param filter vim.lsp.get_clients.Filter?
-local function restart_servers(filter)
-    local clients = vim.lsp.get_clients(filter)
-    ---@type table<integer, {[1]: vim.lsp.Client, [2]: integer[]}>
-    local detach_clients = {}
-    for _, client in ipairs(clients) do
-        detach_clients[client.id] = { client, vim.lsp.get_buffers_by_client_id(client.id) }
-        client:stop()
-    end
-
-    local timer = assert(vim.uv.new_timer(), "cannot create timer")
-    timer:start(
-        500,
-        100,
-        vim.schedule_wrap(function()
-            for old_client_id, tuple in pairs(detach_clients) do
-                local client = tuple[1]
-                local attached_buffers = tuple[2]
-                if client:is_stopped() then
-                    for _, buf in ipairs(attached_buffers) do
-                        vim.lsp.start(client.config, { bufnr = buf })
-                    end
-                    detach_clients[old_client_id] = nil
-                end
-            end
-
-            if next(detach_clients) == nil and not timer:is_closing() then timer:close() end
-        end)
-    )
-end
-
-vim.api.nvim_create_user_command("LspInfo", "checkhealth lsp", {
-    desc = "List LSP clients with their details",
-})
-
-vim.api.nvim_create_user_command("LspStop", function(info)
-    local server_id = parse_int_arg(info.args)
-    if server_id then
-        vim.notify("stoping server with id: " .. server_id)
-        vim.lsp.stop_client(server_id)
-    else
-        vim.notify("stoping all lsp servers")
-        vim.lsp.stop_client(vim.lsp.get_clients())
-    end
-end, {
-    desc = "Stops specified LSP by id",
-    nargs = "?",
-})
-
 vim.api.nvim_create_user_command("LspDetach", function(info)
     local bufnr = vim.api.nvim_get_current_buf()
     local server_id = parse_int_arg(info.args)
@@ -71,20 +22,6 @@ vim.api.nvim_create_user_command("LspDetach", function(info)
     end
 end, {
     desc = "Stops specified LSP by id",
-    nargs = "?",
-})
-
-vim.api.nvim_create_user_command("LspRestart", function(info)
-    local server_id = parse_int_arg(info.args)
-    if server_id then
-        vim.notify("restarting server with id: " .. server_id)
-        restart_servers({ id = server_id })
-    else
-        vim.notify("restarting all lsp servers")
-        restart_servers()
-    end
-end, {
-    desc = "Restarts specified LSPs by id",
     nargs = "?",
 })
 
