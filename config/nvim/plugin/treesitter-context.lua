a087d11dc690e8b2c98e808847fccf2f47a09c4d
Konrad Malik <konrad.malik@gmail.com>
2025-05-13 10:25:20 +0200

    feat: improve code context handling

--- config/nvim/plugin/treesitter-context.lua
+++ config/nvim/plugin/treesitter-context.lua
@@ -1,1 +1,12 @@
-require("treesitter-context").setup()
+local tscontext = require("treesitter-context")
+tscontext.setup({
+    min_window_height = 40,
+    multiline_threshold = 10,
+})
+
+vim.keymap.set(
+    "n",
+    "[c",
+    function() tscontext.go_to_context(vim.v.count1) end,
+    { silent = true, desc = "Jump to co[n]text" }
+)
