5a44f953db336260a12067974e92cc9738fa1afe
Konrad Malik <konrad.malik@gmail.com>
2025-06-30 11:07:20 +0200

    fix: inverse check

--- config/nvim/plugin/treesitter.lua
+++ config/nvim/plugin/treesitter.lua
@@ -24,7 +24,7 @@ vim.api.nvim_create_autocmd("FileType", {
             if not ok then return end
 
             local bufnr = args.buf
-            if vim.api.nvim_buf_is_valid(bufnr) then return end
+            if not vim.api.nvim_buf_is_valid(bufnr) then return end
 
             vim.treesitter.start(bufnr, lang)
 
