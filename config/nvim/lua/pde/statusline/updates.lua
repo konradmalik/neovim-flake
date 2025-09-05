bddaee8bf9a31ca6a0059a22b9f7fc88dac83ca0
Konrad Malik <konrad.malik@gmail.com>
2025-09-05 16:33:03 +0200

    refactor: reuse redraw function

--- config/nvim/lua/pde/statusline/updates.lua
+++ config/nvim/lua/pde/statusline/updates.lua
@@ -1,9 +1,11 @@
+local redraw = vim.schedule_wrap(function() vim.cmd.redrawstatus() end)
+
 local M = {}
 
 M.diagnostics = function()
     vim.api.nvim_create_autocmd("DiagnosticChanged", {
         group = vim.api.nvim_create_augroup("StDiagnostics", { clear = true }),
-        callback = vim.schedule_wrap(function() vim.cmd.redrawstatus() end),
+        callback = redraw,
         desc = "updates statusline every time diagnostics are updated",
     })
 end
@@ -12,7 +14,7 @@ M.git = function()
     vim.api.nvim_create_autocmd("User", {
         group = vim.api.nvim_create_augroup("StGitUpdate", { clear = true }),
         pattern = "GitSignsUpdate",
-        callback = vim.schedule_wrap(function() vim.cmd.redrawstatus() end),
+        callback = redraw,
         desc = "updates statusline every time git status is updated",
     })
 end
