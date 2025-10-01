96c2d5c4e6fc8db5fc2ebf868fc8623566fffb3f
Konrad Malik <konrad.malik@gmail.com>
2025-10-01 11:38:51 +0200

    feat: only enable syntax diagnostics when no lsp diagnostics

--- config/nvim/plugin/syntax-diagnostics.lua
+++ config/nvim/plugin/syntax-diagnostics.lua
@@ -1,7 +1,14 @@
+local ms = vim.lsp.protocol.Methods
 local autocmd_group = vim.api.nvim_create_augroup("editor.treesitter", { clear = true })
 
 vim.api.nvim_create_autocmd({ "FileType", "TextChanged", "InsertLeave" }, {
     desc = "treesitter diagnostics",
     group = autocmd_group,
-    callback = function(args) require("pde.syntax-diagnostics").diagnose(args.buf) end,
+    callback = function(args)
+        local diagnosticProviders =
+            vim.lsp.get_clients({ bufnr = args.buf, method = ms.textDocument_publishDiagnostics })
+        if #diagnosticProviders > 0 then return end
+
+        require("pde.syntax-diagnostics").diagnose(args.buf)
+    end,
 })
