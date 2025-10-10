356df2ea4bb92140626023066e53c465bfbb78e4
Konrad Malik <konrad.malik@gmail.com>
2025-10-10 17:37:17 +0200

    fix: treesitter grammars

--- config/default.nix
+++ config/default.nix
@@ -19,6 +19,8 @@ let
     # manually handle nix templates to avoid IFD
     (callPackage ./nix/lua/pde/debugpy.nix { })
     (callPackage ./nix/lua/pde/skeletons.nix { })
+    # # if we don't use TSInstall, then those are not included in the runtime path
+    # "${inputs.nvim-treesitter}/runtime"
   ];
 in
 symlinkJoin {
