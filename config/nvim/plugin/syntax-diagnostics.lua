1771a9996873c49caa8a05b7071b1ecbb6d0be6b
Konrad Malik <konrad.malik@gmail.com>
2025-10-17 14:32:49 +0200

    fix: diagnostic filter was not valid

--- config/nvim/plugin/syntax-diagnostics.lua
+++ config/nvim/plugin/syntax-diagnostics.lua
@@ -1,12 +1,11 @@
-local ms = vim.lsp.protocol.Methods
 local autocmd_group = vim.api.nvim_create_augroup("editor.treesitter", { clear = true })
 
 vim.api.nvim_create_autocmd({ "FileType", "TextChanged", "InsertLeave" }, {
     desc = "treesitter diagnostics",
     group = autocmd_group,
     callback = function(args)
-        local diagnosticProviders =
-            vim.lsp.get_clients({ bufnr = args.buf, method = ms.textDocument_publishDiagnostics })
+        -- simplification, but assume that if any LSP is connected, then we have diagnostics from that source
+        local diagnosticProviders = vim.lsp.get_clients({ bufnr = args.buf })
         if #diagnosticProviders > 0 then return end
 
         require("pde.syntax-diagnostics").diagnose(args.buf)
