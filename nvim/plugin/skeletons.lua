387dec333a1ca93b668d134ee344ccc0da587770
Konrad Malik <konrad.malik@gmail.com>
2025-12-29 16:43:46 +0100

    fix: don't miss any skeletons

--- nvim/plugin/skeletons.lua
+++ nvim/plugin/skeletons.lua
@@ -5,14 +5,10 @@ local group = vim.api.nvim_create_augroup("personal-skeletons", { clear = true }
 
 local insert_skeleton = function(name) vim.cmd("0r " .. vim.fs.joinpath(skeletons_path, name)) end
 
-vim.api.nvim_create_autocmd({ "BufNewFile" }, {
-    group = group,
-    pattern = ".tmux.sh",
-    callback = function() insert_skeleton(".tmux.sh") end,
-})
-
-vim.api.nvim_create_autocmd({ "BufNewFile" }, {
-    group = group,
-    pattern = ".editorconfig",
-    callback = function() insert_skeleton(".editorconfig") end,
-})
+for name, _ in vim.fs.dir(skeletons_path) do
+    vim.api.nvim_create_autocmd({ "BufNewFile" }, {
+        group = group,
+        pattern = name,
+        callback = function() insert_skeleton(name) end,
+    })
+end
