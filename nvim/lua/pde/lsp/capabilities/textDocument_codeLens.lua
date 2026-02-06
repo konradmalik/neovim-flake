5f6d188dabb4c280e27440b783743b36f10aac34
Konrad Malik <konrad.malik@gmail.com>
2026-02-06 09:41:44 +0100

    refactor: move from deprecated codelens methods

--- nvim/lua/pde/lsp/capabilities/textDocument_codeLens.lua
+++ nvim/lua/pde/lsp/capabilities/textDocument_codeLens.lua
@@ -5,50 +5,25 @@ return {
     attach = function(data)
         local keymapper = require("pde.lsp.keymapper")
 
-        local augroup = data.augroup
         local bufnr = data.bufnr
-        local client = data.client
+        -- it's either client or buffer
+        vim.lsp.codelens.enable(codelens_is_enabled, { bufnr = bufnr })
 
         vim.api.nvim_buf_create_user_command(bufnr, "CodeLensToggle", function()
             codelens_is_enabled = not codelens_is_enabled
-            if not codelens_is_enabled then
-                for _, buf in ipairs(vim.api.nvim_list_bufs()) do
-                    vim.lsp.codelens.clear(client.id, buf)
-                end
-            end
+            vim.lsp.codelens.enable(codelens_is_enabled)
             print("Setting codelens to: " .. tostring(codelens_is_enabled))
         end, {
             desc = "Enable/disable codelens with lsp",
         })
 
-        vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "InsertLeave" }, {
-            group = augroup,
-            buffer = bufnr,
-            callback = function()
-                if not codelens_is_enabled then return end
-                vim.lsp.codelens.refresh({ bufnr = bufnr })
-            end,
-            desc = "Refresh codelens",
-        })
-
         local opts_with_desc = keymapper.opts_for(bufnr)
-
-        vim.api.nvim_buf_create_user_command(
-            bufnr,
-            "CodeLensRefresh",
-            function() vim.lsp.codelens.refresh({ bufnr = bufnr }) end,
-            { desc = "Refresh codelens for the current buffer" }
-        )
         vim.keymap.set("n", "grl", vim.lsp.codelens.run, opts_with_desc("CodeLens run"))
-
-        -- refresh manually right now for a start
-        if codelens_is_enabled then vim.lsp.codelens.refresh({ bufnr = bufnr }) end
     end,
 
-    detach = function(client_id, bufnr)
-        vim.lsp.codelens.clear(client_id, bufnr)
+    detach = function(_, bufnr)
+        vim.lsp.codelens.enable(false, { bufnr = bufnr })
         vim.api.nvim_buf_del_keymap(bufnr, "n", "grl")
         vim.api.nvim_buf_del_user_command(bufnr, "CodeLensToggle")
-        vim.api.nvim_buf_del_user_command(bufnr, "CodeLensRefresh")
     end,
 }
