61207969869d3349b51285dc916e2d9615fdc304
Konrad Malik <konrad.malik@gmail.com>
2026-03-24 12:30:54 +0100

    feat: format regardless of lsp

---
+++ nvim/plugin/format.lua
@@ -0,0 +1,46 @@
+local format_is_enabled = true
+
+---@param bufnr integer?
+local function format(bufnr)
+    bufnr = bufnr or 0
+
+    local clients = vim.lsp.get_clients({
+        bufnr = bufnr,
+        method = vim.lsp.protocol.Methods.textDocument_formatting,
+    })
+
+    -- this is needed for trailing empty lines
+    -- and for formatexpr so just execute this always
+    local view = vim.fn.winsaveview()
+
+    -- lsp if available
+    if #clients > 0 then
+        vim.lsp.buf.format({ bufnr = bufnr, timeout_ms = 2000 })
+    else
+        -- fallback: use formatexpr/formatprg via gq
+        vim.cmd("silent keepjumps normal! gggqG")
+    end
+    -- remove trailing empty lines
+    vim.cmd([[%s/\_s*\%$//e]])
+
+    vim.fn.winrestview(view)
+end
+
+vim.api.nvim_create_user_command("AutoFormatToggle", function()
+    format_is_enabled = not format_is_enabled
+    print("Setting autoformatting to: " .. tostring(format_is_enabled))
+end, {
+    desc = "Enable/disable autoformat with formatexpr globally",
+})
+
+local group = vim.api.nvim_create_augroup("pde-autoformat", { clear = true })
+vim.api.nvim_create_autocmd("BufWritePre", {
+    desc = "AutoFormat on save",
+    group = group,
+    callback = function(args)
+        if not format_is_enabled then return end
+
+        local bufnr = args.buf
+        vim.api.nvim_buf_call(bufnr, function() format(bufnr) end)
+    end,
+})
