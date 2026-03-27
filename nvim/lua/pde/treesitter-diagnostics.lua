60780f4f5444e85825b4d49be038556ef101ad6d
Konrad Malik <konrad.malik@gmail.com>
2026-03-27 15:33:32 +0100

    refactor: reduce nesting in treesitter-diagnostics

--- nvim/lua/pde/treesitter-diagnostics.lua
+++ nvim/lua/pde/treesitter-diagnostics.lua
@@ -45,52 +45,49 @@ function M.diagnose(bufnr)
     -- don't diagnose special buffers
     if vim.bo[bufnr].buftype ~= "" then return end
 
-    local diagnostics = {}
     local parser = vim.treesitter.get_parser(bufnr, nil, { error = false })
-    if parser then
-        parser:parse(false, function(_, trees)
-            if not trees then return end
-            parser:for_each_tree(function(tree, ltree)
-                -- only process trees containing errors
-                if tree:root():has_error() then
-                    for _, node in error_query:iter_captures(tree:root(), bufnr) do
-                        -- collapse nested syntax errors that occur at the exact same position
-                        local parent = node:parent()
-                        if
-                            not parent
-                            or parent:type() ~= "ERROR"
-                            or parent:range() ~= node:range()
-                        then
-                            local lnum, col, end_lnum, end_col = node:range()
+    if not parser then return end
 
-                            -- clamp large syntax error ranges to just the line to reduce noise
-                            if end_lnum > lnum then
-                                end_lnum = lnum + 1
-                                end_col = 0
-                            end
+    local diagnostics = {}
+    parser:parse(false, function(_, trees)
+        if not trees then return end
 
-                            --- @type vim.Diagnostic
-                            local diagnostic = {
-                                source = "treesitter",
-                                lnum = lnum,
-                                end_lnum = end_lnum,
-                                col = col,
-                                end_col = end_col,
-                                message = create_message(node),
-                                code = string.format("%s-syntax", ltree:lang()),
-                                bufnr = bufnr,
-                                namespace = namespace,
-                                severity = vim.diagnostic.severity.ERROR,
-                            }
+        parser:for_each_tree(function(tree, ltree)
+            -- only process trees containing errors
+            if not tree:root():has_error() then return end
 
-                            table.insert(diagnostics, diagnostic)
-                        end
+            for _, node in error_query:iter_captures(tree:root(), bufnr) do
+                -- collapse nested syntax errors that occur at the exact same position
+                local parent = node:parent()
+                if not parent or parent:type() ~= "ERROR" or parent:range() ~= node:range() then
+                    local lnum, col, end_lnum, end_col = node:range()
+
+                    -- clamp large syntax error ranges to just the line to reduce noise
+                    if end_lnum > lnum then
+                        end_lnum = lnum + 1
+                        end_col = 0
                     end
+
+                    --- @type vim.Diagnostic
+                    local diagnostic = {
+                        source = "treesitter",
+                        lnum = lnum,
+                        end_lnum = end_lnum,
+                        col = col,
+                        end_col = end_col,
+                        message = create_message(node),
+                        code = string.format("%s-syntax", ltree:lang()),
+                        bufnr = bufnr,
+                        namespace = namespace,
+                        severity = vim.diagnostic.severity.ERROR,
+                    }
+
+                    table.insert(diagnostics, diagnostic)
                 end
-            end)
+            end
         end)
-        vim.diagnostic.set(namespace, bufnr, diagnostics)
-    end
+    end)
+    vim.diagnostic.set(namespace, bufnr, diagnostics)
 end
 
 return M
