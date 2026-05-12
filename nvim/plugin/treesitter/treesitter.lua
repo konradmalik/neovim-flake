c33491f73fb1f1c351fecae6cdc123b37061dbc0
Konrad Malik <konrad.malik@gmail.com>
2026-05-12 22:21:40 +0200

    fix: remove old school syntax, i don't like it

--- nvim/plugin/treesitter/treesitter.lua
+++ nvim/plugin/treesitter/treesitter.lua
@@ -22,9 +22,6 @@ vim.api.nvim_create_autocmd("FileType", {
         -- if vim.treesitter.query.get(lang, "indents") then
         --     vim.bo[bufnr].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
         -- end
-        -- so we just make sure old-school syntax is enabled
-        -- (it's not required, but old-school indents often use is)
-        vim.bo[bufnr].syntax = "on"
 
         if vim.treesitter.query.get(lang, "folds") then
             for _, win in ipairs(vim.api.nvim_list_wins()) do
