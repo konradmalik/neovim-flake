bb5ad6253a2b4f6cdf5fceb8412f0a5815492f97
Konrad Malik <konrad.malik@gmail.com>
2026-05-22 23:21:18 +0200

    fix: stop using deprecated method

--- nvim/plugin/highlightonyank.lua
+++ nvim/plugin/highlightonyank.lua
@@ -1,6 +1,6 @@
 local group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
-vim.api.nvim_create_autocmd("TextYankPost", {
+vim.api.nvim_create_autocmd({ "TextYankPost", "TextPutPost" }, {
     group = group,
     pattern = "*",
-    callback = function() vim.hl.on_yank({ higroup = "Visual", timeout = 300 }) end,
+    callback = function() vim.hl.hl_op({ higroup = "Visual", timeout = 300 }) end,
 })
