eba4e728d471616ab307fccca8c0df7780054ede
Konrad Malik <konrad.malik@gmail.com>
2026-08-07 10:14:08 +0200

    fix: foldexpr now accepts a function, foldtext from treesiter does not exist

--- nvim/plugin/treesitter/treesitter.lua
+++ nvim/plugin/treesitter/treesitter.lua
@@ -28,8 +28,7 @@ vim.api.nvim_create_autocmd("FileType", {
                 local winbuf = vim.api.nvim_win_get_buf(win)
                 if winbuf == bufnr then
                     vim.wo[win][0].foldmethod = "expr"
-                    vim.wo[win][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
-                    vim.wo[win][0].foldtext = "v:lua.vim.treesitter.foldtext()"
+                    vim.wo[win][0].foldexpr = vim.treesitter.foldexpr
                 end
             end
         end
